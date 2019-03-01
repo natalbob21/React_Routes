ccd8299c8d6b00111438fa46272c91a89b171956
Natalie Dinkins <natalie.dinkins1@gmail.com>
2019-02-28 21:03:23 -0500

    initial commit

---
+++ src/components/Guilty.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../CSS/Guilty.css';
+
+class Guilty extends Component {
+  render() {
+    return (
+      <div id="guilty"></div>
+      
+    );
+  }
+}
+
+export default Guilty;
